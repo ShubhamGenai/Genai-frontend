a4eb21595bf401ae55423ef88615c1422cca45ab
JishnuTP <tpjishnu5@gmail.com>
2025-02-24 11:52:38 +0530

    landing

--- src/pages/auth-pages/Loginlanding.jsx
+++ src/pages/auth-pages/Loginlanding.jsx
@@ -16,21 +16,23 @@ const LoginLandingPage = () => {
             <h2 className="text-6xl font-light mb-6">
               For <span className="font-semibold text-green-600">Employers</span>
             </h2>
-            <p className="text-xl font-light mb-8 max-w-md">
+            <p className="text-xl font-light text-gray-500 mb-8 max-w-md">
               We help companies identify and hire the best developers with the right skills.
             </p>
             <motion.button
               whileHover={{ scale: 1.05 }}
               whileTap={{ scale: 0.95 }}
-              className="px-8 py-3 bg-blue-600 text-white text-lg font-semibold  hover:bg-blue-500 transition-colors"
+              className="px-8 py-3 bg-blue-600 text-white text-lg rounded-lg font-semibold  hover:bg-blue-500 transition-colors"
             >
               Login
             </motion.button>
-            <p className="mt-5 text-lg font-light">
-              Don't have an account?{' '}
-              <a href="#" className="underline font-medium">Contact sales</a>
+            <p className="mt-4 text-md font-normal">
+              Don't have an account?
+            </p>
+            <p className="text-md font-normal">
+              <a href="#" className=" font-bold hover:text-green-600">Contact sales</a>
               {' '}or{' '}
-              <a href="#" className="underline font-medium">Get free trial</a>
+              <a href="#" className=" font-bold hover:text-green-600">Get free trial</a>
             </p>
           </motion.div>
 
@@ -44,19 +46,21 @@ const LoginLandingPage = () => {
             <h2 className="text-6xl font-light mb-6">
               For <span className="font-semibold ">Students</span>
             </h2>
-            <p className="text-xl font-light mb-8 max-w-md">
+            <p className="text-xl font-light mb-8 max-w-md ">
               Join millions of students, practice coding skills, prepare for interviews, and land your dream job.
             </p>
             <motion.button
               whileHover={{ scale: 1.05 }}
               whileTap={{ scale: 0.95 }}
-              className="px-8 py-3 bg-white text-blue-600 text-lg font-semibold g hover:bg-gray-200 transition-colors"
+              className="px-8 py-3 bg-white text-blue-600 text-lg rounded-lg font-semibold g hover:bg-gray-200 transition-colors"
             >
               Login
             </motion.button>
-            <p className="mt-5 text-lg font-light">
-              Don't have an account?{' '}
-              <a href="#" className="underline font-medium">Sign up</a>
+            <p className="mt-4 text-md font-normal">
+              Don't have an account?
+            </p>
+            <p className="text-md font-normal">
+              <a href="#" className=" font-bold hover:text-black">Sign up</a>
             </p>
           </motion.div>
         </div>
